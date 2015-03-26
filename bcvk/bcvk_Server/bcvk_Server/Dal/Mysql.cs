e48d477d3d2814b33dd004e2684093326fce6e0d
Ralph <ralph>
2015-03-26 14:30:02 +0100

    Mysql klasse aangepast

--- bcvk/bcvk_Server/bcvk_Server/Dal/Mysql.cs
+++ bcvk/bcvk_Server/bcvk_Server/Dal/Mysql.cs
@@ -9,7 +9,7 @@ using System.Data;
 namespace Dal
 {
     /// <summary>
-    /// OWNER: Roel Larik 1236830
+    /// OWNER: Roel Larik 1236830 & Ralph
     /// This class creates an instance of a MySQL connection.
     /// </summary>
     public class Mysql
@@ -18,12 +18,6 @@ namespace Dal
         /// MySQLConnection object.
         /// </summary>
         private MySqlConnection connection;
-
-        /// <summary>
-        /// Variables used to establish a connection
-        /// to the database.
-        /// </summary>
-        private string connectionString;
 
         /// <summary>
         /// DataTable to store SELECT statements in.
@@ -36,12 +30,53 @@ namespace Dal
         /// <param name="connectionString"></param>
         public Mysql(string connectionString)
         {
-            // Set the connection string.
-            this.connectionString = connectionString;
-
             // Establish the connection
             connection = new MySqlConnection(connectionString);
-            connection.Open();
+        }
+
+        /// <summary>
+        /// Opens the connection with the database.
+        /// </summary>
+        /// <returns>True if successful, false if not successful</returns>
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Cannot connect to server.");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Invalid username/password.");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Closes the connection with the database.
+        /// </summary>
+        /// <returns>True if successful, false if not successful</returns>
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Database connection could not be closed.");
+                return false;
+            }
         }
 
         /// <summary>
@@ -50,11 +85,15 @@ namespace Dal
         /// <param name="query"></param>
         public void Query(string query)
         {
-            // Set the command with the connection.
-            MySqlCommand cmd = new MySqlCommand(query, connection);
+            if (OpenConnection())
+            {
+                // Set the command with the connection.
+                MySqlCommand cmd = new MySqlCommand(query, connection);
 
-            // Execute the command.
-            cmd.ExecuteNonQuery();
+                // Execute the command.
+                cmd.ExecuteNonQuery();
+            }
+            CloseConnection();
         }
 
         /// <summary>
@@ -64,32 +103,36 @@ namespace Dal
         /// <returns></returns>
         public List<string[]> Select(string command)
         {
-            // Store the DataTable object.
-            table = new DataTable();
-
             // Create a list to store the results.
             List<string[]> list = new List<string[]>();
 
-            // Create the SQL command.
-            MySqlCommand cmd = new MySqlCommand(command, connection);
-
-            // Create a Data Adapter.
-            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
-            table.Clear();
-            adapter.Fill(table);
-
-            // Add the results to the output.
-            for (int i = 0; i < table.Rows.Count; i++)
+            if (OpenConnection())
             {
-                string[] rows = new string[table.Columns.Count];
+                // Store the DataTable object.
+                table = new DataTable();
 
-                for (int x = 0; x < table.Columns.Count; x++)
-                    rows[x] = table.Rows[i][x].ToString();
+                // Create the SQL command.
+                MySqlCommand cmd = new MySqlCommand(command, connection);
 
-                list.Add(rows);
+                // Create a Data Adapter.
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                table.Clear();
+                adapter.Fill(table);
+
+                // Add the results to the output.
+                for (int i = 0; i < table.Rows.Count; i++)
+                {
+                    string[] rows = new string[table.Columns.Count];
+
+                    for (int x = 0; x < table.Columns.Count; x++)
+                        rows[x] = table.Rows[i][x].ToString();
+
+                    list.Add(rows);
+                }
             }
 
             // Return the list with data.
+            CloseConnection();
             return list;
         }
     }
