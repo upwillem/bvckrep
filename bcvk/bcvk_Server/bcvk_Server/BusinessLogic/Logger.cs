109643790bd887a44780f251f54116b219fdd2a7
Ralph <ralph>
2015-04-02 13:02:14 +0200

    Logger activities available without switch statement.

--- bcvk/bcvk_Server/bcvk_Server/BusinessLogic/Logger.cs
+++ bcvk/bcvk_Server/bcvk_Server/BusinessLogic/Logger.cs
@@ -18,38 +18,25 @@ namespace Bu
             ConnectionEnded
         };
 
+        protected static string[] activityString = {
+            "Main account created",
+            "Connection made",
+            "Added contact to connection",
+            "Connection established",
+            "Connection ended"
+        };
+
         public static bool SetLog(int accountId, Activity activity)
         {
-            string activityString = "";
-
-            switch (activity)
-            {
-                case Activity.CreateMainAccount:
-                    activityString = "Main account created";
-                    break;
-                case Activity.ConnectionMade:
-                    activityString = "Connection made";
-                    break;
-                case Activity.ConnectionAddedContact:
-                    activityString = "Added contact to connection";
-                    break;
-                case Activity.ConnectionEstablished:
-                    activityString = "Connection established";
-                    break;
-                case Activity.ConnectionEnded:
-                    activityString = "Connection ended";
-                    break;
-                default:
-                    break;
-            }
-
-            Mysql.Query("INSERT INTO logs (account_id,activity) VALUES(" + accountId + ", '" + activityString + "')");
+            Mysql.Query(String.Format("INSERT INTO logs (account_id,activity) VALUES({0},'{1}')", accountId, activityString[(int)activity]));
 
             return true;
         }
 
-        public static bool SetLog(int accountId, string activity, int contactId)
+        public static bool SetLog(int accountId, Activity activity, int contactId)
         {
+            Mysql.Query(String.Format("INSERT INTO logs (account_id,activity,contact_id) VALUES({0},'{1}',{2})", accountId, activityString[(int)activity], contactId));
+
             return true;
         }
     }
